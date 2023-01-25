4ca3e6d909d6b69059356b200a41a9fb141819c0
alexanderblk <115581300+alexanderblk@users.noreply.github.com>
2023-01-25 13:02:46 +0000

    updated index.js and generateMarkdown.js with code
    Written the code to generate a Readme file

--- index.js
+++ index.js
@@ -5,16 +5,72 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        name: "username",
+        message: "What is your GitHub username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address for additional questions?"
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please provide a brief description of your project:"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What command should be run to install dependencies?",
+        default: "npm i"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "What command should be run to run tests?",
+        default: "npm test"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What does the user need to know about using the repo?"
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "What does the user need to know about contributing to the repo?"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What license would you like to use for your application?",
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+    }
 ];
+
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Successfully created README.md");
+    });
 }
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions).then(answers => {
+        writeToFile("README.md", generateMarkdown(answers));
+    });
 }
 
 // function call to initialize program
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,7 +2,47 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+## Description
+${data.description}
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Running the Tests](#running-the-tests)
+* [Using the Repo](#using-the-repo)
+* [Contributing](#contributing)
+* [License](#license)
+* [Questions](#questions)
+
+## Installation
+To install dependencies, run the following command:
+\`\`\`
+${data.installation}
+\`\`\`
+
+## Usage
+${data.usage}
+
+## Running the Tests
+To run tests, run the following command:
+\`\`\`
+${data.tests}
+\`\`\`
+
+## Using the Repo
+${data.using}
+
+## Contributing
+${data.contributing}
+
+## License
+This project is licensed under the ${data.license} license.
+
+## Questions
+If you have any questions, please reach out to the author at ${data.email}. You can also find the author on GitHub at [${data.username}](https://github.com/${data.username}).
 `;
 }
 
 module.exports = generateMarkdown;
+
+
